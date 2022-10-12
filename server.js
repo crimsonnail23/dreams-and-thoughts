a5cf06b4566e7ed197148df1bf3cd9baf92cff72
night <yseddiq@gmail.com>
2022-10-12 18:35:31 -0400

    got server.js to work

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose =require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({encoded:true}));
+app.use(express.static('public'));
+
+//app.use(require('./routes'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/dreams-and-thoughts',{
+    useNewUrlParser:true,
+    useUnifiedTopology: true
+})
+
+mongoose.set('debug',true);
+
+app.listen(PORT, ()=> console.log(`connected on locahost ${PORT}`))
