b8e2faac2963a8742fc06009bb4b3e2ad06f462f
night <yseddiq@gmail.com>
2022-10-17 01:29:54 -0400

    added GET and POST to user-controllers and user-routes

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,5 +1,15 @@
 const { User } = require('../models');
 
 const userController={
-    
+    //get all users.
+        getAllUsers(req,res){
+            User.find({})
+        },
+        createUser({ body }, res){
+            User.create(body)
+                .then(dbUserData=>res.json(dbUserData))
+                .catch(err=> res.status(400).json(err));
+        }
 }
+
+module.exports = userController;
