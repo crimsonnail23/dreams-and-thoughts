c5373703f032c2b7aba92af0561d9b61503653da
night <yseddiq@gmail.com>
2022-10-17 01:06:34 -0400

    finished working on models

--- models/Thoughts.js
+++ models/Thoughts.js
@@ -1,5 +1,26 @@
 const { Schema, model, Types } = require('mongoose');
 
+const reactionSchema =new Schema({
+    reactionId:{
+        type: Schema.Types.ObjectId,
+        default: ()=> new Types.ObjectId()
+    },
+    reactionBody:{
+        type: String,
+        required: true,
+        max: 280
+    },
+    username:{
+        type:String,
+        required: true
+    },
+    createdAt:{
+        type: Date,
+        default: Date.now
+    }
+}
+)
+
 const ThoughtsSchema= new Schema({
     thoughtText:{
         type: String,
@@ -10,5 +31,14 @@ const ThoughtsSchema= new Schema({
     createdAt:{
         type: Date,
         default: Date.now
-    }
+    },
+    username:{
+        type: String,
+        required: true
+    },
+    reactions:[reactionSchema]
 })
+
+const Thoughts = model('Thoughts', ThoughtsSchema);
+
+module.exports = Thoughts;
