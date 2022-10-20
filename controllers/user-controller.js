ceeecddd2cb577a52ddcd5372d3710525d52498f
night <yseddiq@gmail.com>
2022-10-19 21:29:35 -0400

    added find by id method to user-controller.js

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -10,11 +10,28 @@ const userController={
             .catch((error)=>{console.log(error)
             res.status(500).json(error);})
         },
+        //create new user
         createUser({ body }, res){
             console.log('ln 9 ' + body)
             User.create(body)
                 .then(dbUserData=>res.json(dbUserData))
                 .catch(err=> res.status(400).json(err));
+        },
+        //get User by Id.
+        findUserById({params}, res){
+            User.findOne({ _id: params.id })
+                .select('-__v')
+                .then(dbUserData=>{
+                    if(!dbUserData){
+                        res.status(404).json({message: "no user with that ID"})
+                        return;
+                    }
+                    res.json(dbUserData);
+                })
+                .catch(err=>{
+                    console.log(err);
+                    res.status(400).json(err);
+                })
         }
 }
 
